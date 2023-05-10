f8ddc7b1f77d6d708ca9056ce3d436e43a38edbd
Karl-Johan Bailey <karl@cenedex.com>
2023-05-10 19:29:15 -0400

    doc(trackEvent): comment on method

--- modules/analytics/service.ts
+++ modules/analytics/service.ts
@@ -1,3 +1,7 @@
+/**
+ * @desc Made to abstact analytics provider from the app. This is so we can switch it on the fly
+ */
+
 const trackEvent = (category: string, action: string, label?: string, value?: number) => {
     // Google Analytics tracking code here
     console.log(`Tracking event: ${category}, ${action}, ${label}, ${value}`);
